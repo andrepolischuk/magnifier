5b924c7f3635512228c8b8342a83f9c11040f5da
andrepolischuk <andre.polischuk@gmail.com>
2015-06-24 16:02:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# magnifier
+
+  > Magnifier an image
+
+## Install
+
+```sh
+npm install --save magnifier
+```
+
+## Usage
+
+```js
+var Magnifier = require('magnifier');
+var lens = new Magnifier('.image');
+```
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,153 @@
+
+/**
+ * Module dependencies
+ */
+
+import offset from 'offset';
+
+/**
+ * Expose magnifier
+ *
+ * @param {Element} el
+ * @api public
+ */
+
+export default class Magnifier {
+  constructor(el) {
+    if (typeof el === 'string') el = document.querySelector(el);
+    this.el = el;
+    this.lens = document.createElement('div');
+    this.lens.style.position = 'absolute';
+    this.lens.style.border = '2px solid #eee';
+    this.lens.style.borderRadius = '75px';
+    this.lens.style.height = '150px';
+    this.lens.style.width = '150px';
+    this.lens.style.backgroundColor = '#fff';
+    this.lens.style.backgroundRepeat = 'no-repeat';
+    this.lens.style.overflow = 'hidden';
+    this.lens.style.visibility = 'hidden';
+    this.className = 'magnifier';
+    insertAfter(this.lens, this.el);
+    this.move = this.onmove.bind(this);
+    this.show();
+    this.getRatio();
+    this.bind();
+  }
+
+  getRatio() {
+    const orig = document.createElement('img');
+    orig.style.position = 'absolute';
+    orig.style.width = 'auto';
+    orig.style.visibility = 'hidden';
+    orig.src = this.el.src;
+
+    orig.onload = () => {
+      this.ratioX = orig.offsetWidth / this.el.offsetWidth;
+      this.ratioY = orig.offsetHeight / this.el.offsetHeight;
+      orig.parentNode.removeChild(orig);
+      this.hide();
+      this.lens.style.visibility = 'visible';
+      this.lens.style.backgroundImage = `url(${this.el.src})`;
+    };
+
+    this.lens.appendChild(orig);
+  }
+
+  onmove(event) {
+    event = event.type === 'touchmove' ? event.changedTouches[0] : event;
+    const {pageX, pageY} = event;
+    const {ratioX, ratioY} = this;
+    const {left, top} = offset(this.el);
+
+    if (!isInside(this.el, event)) return this.hide();
+
+    const imageX = (left - pageX) * ratioX + this.lens.offsetWidth / 2;
+    const imageY = (top - pageY) * ratioY + this.lens.offsetHeight / 2;
+    let x = pageX - this.lens.offsetWidth / 2;
+    let y = pageY - this.lens.offsetHeight / 2;
+
+    if (!isStatic(this.el)) {
+      x -= left;
+      y -= top;
+    }
+
+    this.show();
+    this.lens.style.left = `${x}px`;
+    this.lens.style.top = `${y}px`;
+    this.lens.style.backgroundPosition = `${imageX}px ${imageY}px`;
+  }
+
+  bind() {
+    this.el.addEventListener('mousemove', this.move, false);
+    this.el.addEventListener('touchmove', this.move, false);
+    this.lens.addEventListener('mousemove', this.move, false);
+    this.lens.addEventListener('touchmove', this.move, false);
+    return this;
+  }
+
+  unbind() {
+    this.el.removeEventListener('mousemove', this.move, false);
+    this.el.removeEventListener('touchmove', this.move, false);
+    this.lens.removeEventListener('mousemove', this.move, false);
+    this.lens.removeEventListener('touchmove', this.move, false);
+    return this;
+  }
+
+  show() {
+    this.lens.style.display = 'block';
+    return this;
+  }
+
+  hide() {
+    this.lens.style.display = 'none';
+    return this;
+  }
+}
+
+/**
+ * Insert after
+ *
+ * @param {Element} el
+ * @param {Element} ref
+ * @return {Element}
+ * @api private
+ */
+
+function insertAfter(el, ref) {
+  const parent = ref.parentNode;
+  let next = ref.nextSibling;
+  while (next && next.nodeType > 1) next = next.nextSibling;
+  if (!next) return parent.appendChild(el);
+  return parent.insertBefore(el, next);
+}
+
+/**
+ * Detect pointer event is inside
+ *
+ * @param {Element} el
+ * @param {Event} event
+ * @return {Boolean}
+ * @api private
+ */
+
+function isInside(el, event) {
+  const {pageX, pageY} = event;
+  const {left, top} = offset(el);
+  const {offsetWidth, offsetHeight} = el;
+  return pageX >= left && pageX <= left + offsetWidth &&
+    pageY >= top && pageY <= top + offsetHeight;
+}
+
+/**
+ * Detect element is static
+ *
+ * @param {Element} el
+ * @return {Boolean}
+ * @api private
+ */
+
+function isStatic(el) {
+  const {left, top} = offset(el);
+  const {offsetLeft, offsetTop} = el;
+  return Math.ceil(left) === offsetLeft && Math.ceil(top) === offsetTop;
+}
