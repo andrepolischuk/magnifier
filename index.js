7834365c5c9ca653f6f3f78cf77f2cc5db0f5ed4
andrepolischuk <andre.polischuk@gmail.com>
2015-10-03 01:03:57 +0300

    Add methods for set lens configuration

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ import insertAfter from 'insert-after';
 import isPointerInside from 'is-pointer-inside';
 
 export default class Magnifier {
+  props = {
+    height: 150,
+    width: 150,
+    backgroundColor: '#fff',
+    borderColor: '#eee',
+    borderRadius: 75,
+    borderWidth: 2
+  };
+
   constructor(el) {
     if (typeof el === 'string') el = document.querySelector(el);
     this.el = el;
     this.lens = document.createElement('div');
     this.lens.style.position = 'absolute';
-    this.lens.style.border = '2px solid #eee';
-    this.lens.style.borderRadius = '75px';
-    this.lens.style.height = '150px';
-    this.lens.style.width = '150px';
-    this.lens.style.backgroundColor = '#fff';
     this.lens.style.backgroundRepeat = 'no-repeat';
+    this.lens.style.borderStyle = 'solid';
     this.lens.style.overflow = 'hidden';
     this.lens.style.visibility = 'hidden';
+    Object.keys(this.props).forEach(prop => this.setStyle(prop, this.props[prop]));
     this.lens.className = 'magnifier';
     insertAfter(this.lens, this.el);
     this.show();
@@ -52,13 +58,13 @@ export default class Magnifier {
     const {pageX, pageY} = event;
     const {left, top} = offset(this.el);
     const {offsetLeft, offsetTop, offsetWidth, offsetHeight} = this.el;
-    const {offsetWidth: lensWidth, offsetHeight: lensHeight} = this.lens;
+    const {width, height, borderWidth} = this.props;
     const ratioX = this.imageWidth / offsetWidth;
     const ratioY = this.imageHeight / offsetHeight;
-    const imageX = (left - pageX) * ratioX + lensWidth / 2 - 2;
-    const imageY = (top - pageY) * ratioY + lensHeight / 2 - 2;
-    const x = pageX - lensWidth / 2 - (left !== offsetLeft ? left - offsetLeft : 0);
-    const y = pageY - lensHeight / 2 - (top !== offsetTop ? top - offsetTop : 0);
+    const imageX = (left - pageX) * ratioX + width / 2 - borderWidth;
+    const imageY = (top - pageY) * ratioY + height / 2 - borderWidth;
+    const x = pageX - width / 2 - (left !== offsetLeft ? left - offsetLeft : 0);
+    const y = pageY - height / 2 - (top !== offsetTop ? top - offsetTop : 0);
     this.lens.style.left = `${x}px`;
     this.lens.style.top = `${y}px`;
     this.lens.style.backgroundPosition = `${imageX}px ${imageY}px`;
@@ -87,6 +93,45 @@ export default class Magnifier {
     this.lens.removeEventListener('mouseleave', this.onend, false);
     this.lens.removeEventListener('touchmove', this.onmove, false);
     this.lens.removeEventListener('touchend', this.onend, false);
+    return this;
+  }
+
+  height(n) {
+    return this.setProp('height', n);
+  }
+
+  width(n) {
+    return this.setProp('width', n);
+  }
+
+  backgroundColor(color) {
+    return this.setProp('backgroundColor', color);
+  }
+
+  borderColor(color) {
+    return this.setProp('borderColor', color);
+  }
+
+  borderRadius(n) {
+    return this.setProp('borderRadius', n);
+  }
+
+  borderWidth(n) {
+    return this.setProp('borderWidth', n);
+  }
+
+  setProp(prop, value) {
+    this.props[prop] = value;
+    this.setStyle(prop, value);
+    return this;
+  }
+
+  setStyle(prop, value) {
+    this.lens.style[prop] = typeof value === 'number' ? `${value}px` : value;
+  }
+
+  className(name) {
+    this.lens.className = name;
     return this;
   }
 
